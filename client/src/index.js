9e91fa436dcccc375ea2484095d402e50e4d2f61
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2023-11-23 18:20:37 +0530

    fix: :bug: wrapping
    wrapped app component in router component

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ContextProvider } from "./context/Context";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
