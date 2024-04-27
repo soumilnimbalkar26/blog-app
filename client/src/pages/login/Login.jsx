a66880babf06261fb73a3f783ee09b06d867a65e
soumilnimbalkar26 <soumilnimblkar@gmail.com>
2024-04-27 08:38:55 +0530

    changes made in css for login and signup form

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -25,31 +25,33 @@ export default function Login() {
 
   return (
     <div className="login">
-      <span className="loginTitle">Login</span>
-      <form className="loginForm" onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input
-          type="text"
-          className="loginInput"
-          placeholder="Enter your username..."
-          ref={userRef}
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          className="loginInput"
-          placeholder="Enter your password..."
-          ref={passwordRef}
-        />
-        <button className="loginButton" type="submit" disabled={isFetching}>
-          Login
+      <div className="login-card">
+        <div className="loginTitle">Login</div>
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input
+            type="text"
+            className="loginInput"
+            placeholder="Enter your username..."
+            ref={userRef}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            className="loginInput"
+            placeholder="Enter your password..."
+            ref={passwordRef}
+          />
+          <button className="loginButton" type="submit" disabled={isFetching}>
+            Login
+          </button>
+        </form>
+        <button className="loginRegisterButton">
+          <Link className="link" to="/register">
+            Register
+          </Link>
         </button>
-      </form>
-      <button className="loginRegisterButton">
-        <Link className="link" to="/register">
-          Register
-        </Link>
-      </button>
+      </div>
     </div>
   );
 }
